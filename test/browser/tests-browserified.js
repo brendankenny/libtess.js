b70616bb9fca8dedc8607dae97281ccf8cb93cdd
Brendan Kenny <bckenny@gmail.com>
2014-10-26 23:32:15 -0700

    add poly2tri dude.dat test

--- test/browser/tests-browserified.js
+++ test/browser/tests-browserified.js
@@ -518,7 +518,7 @@ var createPlaneRotation = common.createPlaneRotation;
 var basetess = require('./expectations/libtess.baseline.js');
 
 var rfolder = require('./rfolder.js');
-var geometryFiles = {"hourglass": require("./geometry/hourglass.js"),"letter-e": require("./geometry/letter-e.js"),"two-opposite-triangles": require("./geometry/two-opposite-triangles.js"),"two-traingles": require("./geometry/two-traingles.js")};
+var geometryFiles = {"hourglass": require("./geometry/hourglass.js"),"letter-e": require("./geometry/letter-e.js"),"poly2tri-dude": require("./geometry/poly2tri-dude.js"),"two-opposite-triangles": require("./geometry/two-opposite-triangles.js"),"two-traingles": require("./geometry/two-traingles.js")};
 var geometries = Object.keys(geometryFiles).map(function(filename) {
   return geometryFiles[filename];
 });
@@ -690,7 +690,7 @@ function tessellate(tess, contours, outputType, provideNormal, normal,
   return resultVerts;
 }
 
-},{"./common.js":5,"./expectations/libtess.baseline.js":6,"./geometry/hourglass.js":7,"./geometry/letter-e.js":8,"./geometry/two-opposite-triangles.js":9,"./geometry/two-traingles.js":10,"./rfolder.js":4,"chai":undefined}],4:[function(require,module,exports){
+},{"./common.js":5,"./expectations/libtess.baseline.js":6,"./geometry/hourglass.js":7,"./geometry/letter-e.js":8,"./geometry/poly2tri-dude.js":9,"./geometry/two-opposite-triangles.js":10,"./geometry/two-traingles.js":11,"./rfolder.js":4,"chai":undefined}],4:[function(require,module,exports){
 
 },{}],5:[function(require,module,exports){
 /* jshint node: true */
@@ -990,6 +990,159 @@ module.exports = {
 };
 
 },{}],9:[function(require,module,exports){
+/**
+ * Poly2Tri Copyright (c) 2009-2010, Poly2Tri Contributors
+ * http://code.google.com/p/poly2tri/
+ *
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of Poly2Tri nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* jshint node: true */
+
+module.exports = {
+  // NOTE(bckenny): I believe the source of this is the poly2tri library.
+  name: 'poly2tri dude.dat test',
+  value: [
+    // torso
+    [
+      280.35714, 648.79075, 0,
+      286.78571, 662.8979, 0,
+      263.28607, 661.17871, 0,
+      262.31092, 671.41548, 0,
+      250.53571, 677.00504, 0,
+      250.53571, 683.43361, 0,
+      256.42857, 685.21933, 0,
+      297.14286, 669.50504, 0,
+      289.28571, 649.50504, 0,
+      285, 631.6479, 0,
+      285, 608.79075, 0,
+      292.85714, 585.21932, 0,
+      306.42857, 563.79075, 0,
+      323.57143, 548.79075, 0,
+      339.28571, 545.21932, 0,
+      357.85714, 547.36218, 0,
+      375, 550.21932, 0,
+      391.42857, 568.07647, 0,
+      404.28571, 588.79075, 0,
+      413.57143, 612.36218, 0,
+      417.14286, 628.07647, 0,
+      438.57143, 619.1479, 0,
+      438.03572, 618.96932, 0,
+      437.5, 609.50504, 0,
+      426.96429, 609.86218, 0,
+      424.64286, 615.57647, 0,
+      419.82143, 615.04075, 0,
+      420.35714, 605.04075, 0,
+      428.39286, 598.43361, 0,
+      437.85714, 599.68361, 0,
+      443.57143, 613.79075, 0,
+      450.71429, 610.21933, 0,
+      431.42857, 575.21932, 0,
+      405.71429, 550.21932, 0,
+      372.85714, 534.50504, 0,
+      349.28571, 531.6479, 0,
+      346.42857, 521.6479, 0,
+      346.42857, 511.6479, 0,
+      350.71429, 496.6479, 0,
+      367.85714, 476.6479, 0,
+      377.14286, 460.93361, 0,
+      385.71429, 445.21932, 0,
+      388.57143, 404.50504, 0,
+      360, 352.36218, 0,
+      337.14286, 325.93361, 0,
+      330.71429, 334.50504, 0,
+      347.14286, 354.50504, 0,
+      337.85714, 370.21932, 0,
+      333.57143, 359.50504, 0,
+      319.28571, 353.07647, 0,
+      312.85714, 366.6479, 0,
+      350.71429, 387.36218, 0,
+      368.57143, 408.07647, 0,
+      375.71429, 431.6479, 0,
+      372.14286, 454.50504, 0,
+      366.42857, 462.36218, 0,
+      352.85714, 462.36218, 0,
+      336.42857, 456.6479, 0,
+      332.85714, 438.79075, 0,
+      338.57143, 423.79075, 0,
+      338.57143, 411.6479, 0,
+      327.85714, 405.93361, 0,
+      320.71429, 407.36218, 0,
+      315.71429, 423.07647, 0,
+      314.28571, 440.21932, 0,
+      325, 447.71932, 0,
+      324.82143, 460.93361, 0,
+      317.85714, 470.57647, 0,
+      304.28571, 483.79075, 0,
+      287.14286, 491.29075, 0,
+      263.03571, 498.61218, 0,
+      251.60714, 503.07647, 0,
+      251.25, 533.61218, 0,
+      260.71429, 533.61218, 0,
+      272.85714, 528.43361, 0,
+      286.07143, 518.61218, 0,
+      297.32143, 508.25504, 0,
+      297.85714, 507.36218, 0,
+      298.39286, 506.46932, 0,
+      307.14286, 496.6479, 0,
+      312.67857, 491.6479, 0,
+      317.32143, 503.07647, 0,
+      322.5, 514.1479, 0,
+      325.53571, 521.11218, 0,
+      327.14286, 525.75504, 0,
+      326.96429, 535.04075, 0,
+      311.78571, 540.04075, 0,
+      291.07143, 552.71932, 0,
+      274.82143, 568.43361, 0,
+      259.10714, 592.8979, 0,
+      254.28571, 604.50504, 0,
+      251.07143, 621.11218, 0,
+      250.53571, 649.1479, 0,
+      268.1955, 654.36208, 0
+    ],
+    // head hole
+    [
+      325, 437, 0,
+      320, 423, 0,
+      329, 413, 0,
+      332, 423, 0
+    ],
+    // chest hole
+    [
+      320.72342, 480, 0,
+      338.90617, 465.96863, 0,
+      347.99754, 480.61584, 0,
+      329.8148, 510.41534, 0,
+      339.91632, 480.11077, 0,
+      334.86556, 478.09046, 0
+    ]
+  ]
+};
+
+},{}],10:[function(require,module,exports){
 /* jshint node: true */
 
 module.exports = {
@@ -1010,7 +1163,7 @@ module.exports = {
   ]
 };
 
-},{}],10:[function(require,module,exports){
+},{}],11:[function(require,module,exports){
 /* jshint node: true */
 
 module.exports = {
