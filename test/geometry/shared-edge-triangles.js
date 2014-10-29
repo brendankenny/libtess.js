7ffe6b76b9f9a40ca82fc90737e1fedb8b5f85cc
Brendan Kenny <bckenny@gmail.com>
2014-10-28 18:11:37 -0700

    add another contours with shared edges test

---
+++ test/geometry/shared-edge-triangles.js
@@ -0,0 +1,20 @@
+/* jshint node: true */
+
+module.exports = {
+  // two triangles with a partially-shared edge
+  name: 'Shared-edge triangles',
+  value: [
+    // anticlockwise
+    [
+      0, 2, 0,
+      5, 2, 0,
+      2, 4, 0
+    ],
+    // clockwise
+    [
+      1, 2, 0,
+      7, 2, 0,
+      4, 0, 0
+    ]
+  ]
+};
